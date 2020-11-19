824875753a2469a3d47a59bc932d444e35798965
Ivan Galakhov <igalakhov.nyc@gmail.com>
2020-11-19 17:18:09 -0600

    step0 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate rustyline;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+
 fn main() {
-    println!("Hello, world!");
+    // main input/output loop
+    let mut rl = Editor::<()>::new();
+
+    loop {
+        let readline = rl.readline("user> ");
+
+        match readline {
+            Ok(line) => {
+                if line.len() == 0 {
+                    continue;
+                }
+
+                println!("{}", line);
+            }
+            Err(ReadlineError::Interrupted) => break, // also exit on ctrl-c
+            Err(ReadlineError::Eof) => break,
+            Err(err) => {
+                println!("Input error: {:?}", err);
+                break;
+            }
+        }
+    }
 }
